1a43cfbe9310f8cdf9a90d30a87ae232c56a8077
Dina Saunders <dina_saunders@mac.com>
2021-01-16 18:57:25 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "./pages/Search";
+// import Saved from "./pages/Saved";
+
+import Wrapper from "./components/Wrapper";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+
+function App() {
+  return (
+    <Router>
+    <div>
+    <Nav />
+    <Jumbotron />
+    <Wrapper>
+      <Route exact path ="/" component={Search} />
+      <Route exact path ="/search" component={Search} />
+      {/* <Route exact path ="/saved" component={Saved} /> */}
+      </Wrapper>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
