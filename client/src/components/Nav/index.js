b8b0ac0de0381c32196fd33bf6a88b72f81dd1ae
Dina Saunders <dina_saunders@mac.com>
2021-01-22 17:48:03 -0500

    added responsive toggler

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -26,8 +26,11 @@ function Nav() {
     <nav className="navbar navbar-light navbar-expand-lg fixed-top" style={{backgroundColor: pos === "top" ? "transparent" : "white" }}>
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">Google Books</NavLink>
-        <div className="navbarNav">
-          <ul className="navbar-nav">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarNav">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <NavLink
                 to="/search"
