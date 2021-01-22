5e70fdb040932fc3b3cd632712b0822eebd4cd65
Dina Saunders <dina_saunders@mac.com>
2021-01-22 17:41:52 -0500

    added conditional style properties for scrolling

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect, } from "react";
 import { NavLink } from "react-router-dom";
 import "./style.css";
 
 function Nav() {
+
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+
+    const [pos, setPos] = useState("top")
+
+    useEffect (() =>{
+        document.addEventListener("scroll", e => {
+            let scrolled = document.scrollingElement.scrollTop;
+            if (scrolled >= 5){
+               setPos("moved")
+            } else {
+               setPos("top")
+            }
+        })
+    },[])
+
   return (
-    <nav className="navbar navbar-dark navbar-expand-lg fixed-top">
+    <nav className="navbar navbar-light navbar-expand-lg fixed-top" style={{backgroundColor: pos === "top" ? "transparent" : "white" }}>
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">Google Books</NavLink>
         <div className="navbarNav">
